1e4306f257fa556693819ac0f209514ca8f30859
dishakalgutkar28 <dishamkalgutkar2003@gmail.com>
2026-03-12 13:54:32 +0530

    Add temporary admin reset endpoint

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ const notificationRoutes = require("./routes/notificationRoutes");
 const reportExportRoutes = require("./routes/reportExportRoutes");
 const attachmentRoutes = require("./routes/attachmentRoutes");
 const retestRoutes = require("./routes/retestRoutes");
+const fixAdminRoute = require("./routes/fixAdminRoute");
 
 const db = require('./config/db');
 const logger = require('./utils/logger');
@@ -103,6 +104,7 @@ app.use("/api", notificationRoutes);
 app.use("/api", reportExportRoutes);
 app.use("/api", attachmentRoutes);
 app.use("/api", retestRoutes);
+app.use("/api", fixAdminRoute);
 
 logger.info("All routes loaded successfully");
 console.log("📝 Comment routes available at:");
